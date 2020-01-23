aca3006b7cb8d3148fd607b266088b783527d265
Derrick Shaw <dajshaw77@gmail.com>
2020-01-23 13:50:57 -0600

    added filtering for menu items

--- foodvalet/lib/routes.js
+++ foodvalet/lib/routes.js
@@ -5,13 +5,13 @@ if (Meteor.isClient) {
     Accounts.onLogout(function(){
         FlowRouter.go('home');
     });
-}
+},
+
 FlowRouter.triggers.enter([function(context, redirect){
     if(!Meteor.userId()) {
         FlowRouter.go('home');
-    }
+    },
 }]);
-
 
 FlowRouter.route('/', {
     name: 'home',
@@ -29,12 +29,20 @@ FlowRouter.route('/recipe-book', {
     action() {
         GAnalytics.pageview();
         BlazeLayout.render('MainLayout', {main: 'Recipes'});
-    }
+    },
 });
+
 FlowRouter.route('/recipe/:id', {
     name: 'recipe-book',
     action() {
         GAnalytics.pageview();
         BlazeLayout.render('MainLayout', {main: 'Recipes'});
+    },
+});
+
+FlowRouter.route('/menu', {
+    name: 'menu',
+    action() {
+        BlazeLayout.render('MainLayout', {main: 'Menu'});
     },
 });
