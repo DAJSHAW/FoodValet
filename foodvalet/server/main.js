6f40308b2e55dba243fe977e0bf0da63b800033c
Derrick Shaw <dajshaw77@gmail.com>
2020-01-23 07:27:02 -0600

    fixed more errors on server

--- foodvalet/server/main.js
+++ foodvalet/server/main.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(function () {
+
+import '../collections/Recipes.js'
+import './publish'
+
+Meteor.startup(() => {
   // code to run on server at startup
   console.log(Meteor.settings.hello);
 });
